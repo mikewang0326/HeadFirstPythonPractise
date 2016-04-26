417d05a4e5c9d147b856a99cfc04f1a9be678934
MikeWang <hi.from.mikewang@gmail.com>
2016-04-26 11:39:39 +0800

    add:list comprehension

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 1, official resources download
    website:http://www.headfirstlabs.com/books/hfpython/
 
+2, chapter name
+Meet Python: Everyone Loves Lists
+Sharing Your Code: Modules of Functions
+Files and Exceptions: Dealing with Errors
+Persistence: Saving Data to Files
+Comprehending Data: Work That Data!
+Custom Data Objects: Bundling Code with Data
+Web Development: Putting it All Together
+Mobile App Development: Small Devices
+Manage Your Data: Handling Input
+Scaling Your Webapp: Getting Real
+Dealing with Complexity: Data Wrangling
 
---
+++ chapter5:process data/processdata.py
@@ -0,0 +1,73 @@
+import os
+
+print("path " + os.path.abspath('.'))
+
+os.getcwd()
+
+os.chdir('./')
+
+os.getcwd()
+
+def sanitize(time_string):
+    if ("-" in time_string):
+        splitter = '-'
+        (mins, seconds) = time_string.split(splitter)
+        return mins + "." + seconds
+    elif (':' in time_string):
+        splitter = ":"
+        (mins, seconds) = time_string.split(splitter)
+        return mins + "." + seconds
+    else:
+        return time_string
+
+if os.path.exists('james.txt'):
+    data = open('james.txt')
+    for line in data:
+        james = line.strip().split(",")
+        print('james       ', james)
+    cleaned_james = []
+    for each in james:
+        cleaned_james.append(sanitize(each))
+    print('clean_james ', cleaned_james)
+
+    '''列表推导'''
+    quick_clean_james = [sanitize(each_t) for each_t in james]
+    print('quick_clean_james', quick_clean_james)
+
+    sorted_james = sorted(cleaned_james)
+    print('sorted_james', sorted_james)
+
+    data.close()
+
+else:
+    print('no file exist')
+
+
+if os.path.exists('julie.txt'):
+    data = open('julie.txt')
+    for line in data:
+        julie = line.strip().split(",")
+        print('julie       ', julie)
+        sorted_julie = sorted(julie)
+        print('sorted_julie', sorted_julie)
+
+    data.close()
+
+else:
+    print('no file exist')
+
+if os.path.exists('mikey.txt'):
+    data = open('mikey.txt')
+    for line in data:
+        mikey = line.strip().split(",")
+        print('mikey       ', mikey)
+        sorted_mikey = sorted(mikey)
+        print('sorted_mikey', sorted_mikey)
+    data.close()
+
+else:
+    print('no file exist')
+
+
+
+
